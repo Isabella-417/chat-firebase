422a7e35bad1ba950a03785fb4a3a83cfb3c3fe3
Isabella Serna <isabella.serna@Isabellas-MacBook-Pro.local>
2023-01-18 13:55:09 -0500

    Fix file type

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import styled from 'styled-components'
 
 import { Button, Greeting } from '../components'
 
 import Google from '../assets/images/google-logo.svg'
 
-export const Chat = () => {
+export const Chat = (): JSX.Element => {
   return (
     <Background>
       <Greeting
